d0860cb3c159d5553a5be91e8d278d94ee803f89
Akshansh Gusain <akshanshgusain@gmail.com>
2023-07-29 11:36:13 +0530

    test: add tests for non-existent templates

--- render_test.go
+++ render_test.go
@@ -19,10 +19,19 @@ func TestRender_Page(t *testing.T) {
 	if err != nil {
 		t.Error("error rendering page", err)
 	}
+	err = te.Page(w, r, "no-file", nil, nil)
+	if err != nil {
+		t.Error("error rendering non-existent go template", err)
+	}
 
 	te.TemplateEngine = "jet"
 	err = te.Page(w, r, "home", nil, nil)
 	if err != nil {
 		t.Error("error rendering page", err)
+	}
+
+	err = te.Page(w, r, "no-file", nil, nil)
+	if err != nil {
+		t.Error("error rendering non-existent jet template", err)
 	}
 }
