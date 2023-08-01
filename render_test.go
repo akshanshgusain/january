82f67b762a541dc2d52f271debb9352eadf8fda4
Akshansh Gusain <akshanshgusain@gmail.com>
2023-08-01 11:00:43 +0530

    test: add table tests

--- render_test.go
+++ render_test.go
@@ -6,39 +6,41 @@ import (
 	"testing"
 )
 
+var pd = []struct {
+	name           string
+	templateEngine string
+	template       string
+	errorExpected  bool
+	errorMessage   string
+}{
+	{"go_page", "go", "home", false, "error rendering go template"},
+	{"go_page_no_template", "go", "no=file", true, "error rendering non-existent go template, when one is expected"},
+	{"jet_page", "jet", "home", false, "error rendering jet template"},
+	{"jet_page_no_template", "jet", "no=file", true, "error rendering non-existent jet template, when one is expected"},
+	{"invalid_template_engine", "na", "home", true, "no error returned while rendering with invalid template engine"},
+}
+
 func TestRender_Page(t *testing.T) {
-	r, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-		t.Error(err)
-	}
-	w := httptest.NewRecorder()
-	te.TemplateEngine = "go"
-	te.RootPath = "./testData"
 
-	err = te.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("error rendering page", err)
-	}
-	err = te.Page(w, r, "no-file", nil, nil)
-	if err == nil {
-		t.Error("error rendering non-existent go template", err)
-	}
+	for _, c := range pd {
+		r, err := http.NewRequest("GET", "/", nil)
+		if err != nil {
+			t.Error(err)
+		}
+		w := httptest.NewRecorder()
+		te.TemplateEngine = c.templateEngine
+		te.RootPath = "./testData"
 
-	te.TemplateEngine = "jet"
-	err = te.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("error rendering page", err)
-	}
-
-	err = te.Page(w, r, "no-file", nil, nil)
-	if err == nil {
-		t.Error("error rendering non-existent jet template", err)
-	}
-
-	te.TemplateEngine = ""
-	err = te.Page(w, r, "home", nil, nil)
-	if err == nil {
-		t.Error("no error returned while rendering with invalid template engine", err)
+		err = te.Page(w, r, c.template, nil, nil)
+		if c.errorExpected {
+			if err == nil {
+				t.Errorf("%s: %s", c.name, c.errorMessage)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%s: %s: %s", c.name, c.errorMessage, err.Error())
+			}
+		}
 	}
 }
 
