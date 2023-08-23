38857f619231ce67fa8f4af627ca456849ebb27a
Akshansh Gusain <akshanshgusain@gmail.com>
2023-08-23 11:42:17 +0530

    feat(helpers): add random string generator

--- helpers.go
+++ helpers.go
@@ -1,8 +1,23 @@
 package january
 
-import "os"
+import (
+	"crypto/rand"
+	"os"
+)
 
 // CreateDirIfNotExist TODO: create dirs if not exist
+
+const randomString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+
+func (j *January) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomString)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+	return string(s)
+}
 
 func (j *January) CreateDirIfNotExist(path string) error {
 	// default permission : The folders will be readable and executed by others, but writable by the user only
