034eb9591f97921b713c0ca1e75d097145078c14
Akshansh Gusain <akshanshgusain@gmail.com>
2023-09-21 18:56:29 +0530

    feat: add migrateDownAll, add migrate command

--- cmd/cli/helpers.go
+++ cmd/cli/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
 	"os"
 )
@@ -17,4 +18,34 @@ func setup() {
 
 	j.RootPath = path
 	j.DB.DataType = os.Getenv("DATABASE_TYPE")
+}
+
+func getDSN() string {
+	dbType := j.DB.DataType
+
+	if dbType == "pgx" {
+		dbType = "postgres"
+	}
+
+	if dbType == "postgres" {
+		var dsn string
+		if os.Getenv("DATABASE_PASS") != "" {
+			dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+				os.Getenv("DATABASE_USER"),
+				os.Getenv("DATABASE_PASS"),
+				os.Getenv("DATABASE_HOST"),
+				os.Getenv("DATABASE_PORT"),
+				os.Getenv("DATABASE_NAME"),
+				os.Getenv("DATABASE_SSL_MODE"))
+		} else {
+			dsn = fmt.Sprintf("postgres://%s@%s:%s/%s?sslmode=%s",
+				os.Getenv("DATABASE_USER"),
+				os.Getenv("DATABASE_HOST"),
+				os.Getenv("DATABASE_PORT"),
+				os.Getenv("DATABASE_NAME"),
+				os.Getenv("DATABASE_SSL_MODE"))
+		}
+		return dsn
+	}
+	return "mysql://" + j.BuildDSN()
 }
