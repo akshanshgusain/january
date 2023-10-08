932ed8ac43b8aaa5ac1f43f7780fa40d615ef77c
Akshansh Gusain <akshanshgusain@gmail.com>
2023-10-08 12:33:00 +0530

    feat: add WriteJson response write to January

---
+++ responseUtils.go
@@ -0,0 +1,28 @@
+package january
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (j *January) WriteJson(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
