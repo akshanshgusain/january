81405a79cb921e4fa094476031b29f6500785d0d
Akshansh Gusain <akshanshgusain@gmail.com>
2023-10-10 16:15:36 +0530

    feat(cli/make.go): add make model <name> command to cli, creates a stub model with CRUD in the data directory

--- cmd/cli/make.go
+++ cmd/cli/make.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/gertd/go-pluralize"
 	"github.com/iancoleman/strcase"
 	"io/ioutil"
 	"strings"
@@ -53,12 +54,50 @@ func doMake(arg2, arg3 string) error {
 
 		handler := string(data)
 		handler = strings.ReplaceAll(handler, "$HANDLERNAME$", strcase.ToCamel(arg3))
+
+		// write the new handler
 		err = ioutil.WriteFile(fileName, []byte(handler), 0644)
 		if err != nil {
 			exitGracefully(err)
 		}
 
-		// TODO: write the new handler
+	case "model":
+		if arg3 == "" {
+			exitGracefully(errors.New("you must give the model a name"))
+		}
+
+		// copy some template file to the new handler
+		data, err := templateFS.ReadFile("templates/data/model.go.txt")
+		if err != nil {
+			exitGracefully(err)
+		}
+
+		model := string(data)
+
+		plur := pluralize.NewClient()
+		var modelName = arg3
+		var tableName = arg3
+
+		if plur.IsPlural(arg3) {
+			modelName = plur.Singular(arg3)
+			tableName = strings.ToLower(tableName)
+		} else {
+			tableName = strings.ToLower(plur.Plural(arg3))
+		}
+
+		// build a file name for the new handler
+		fileName := j.RootPath + "/data/" + strings.ToLower(modelName) + ".go"
+		if fileExists(fileName) {
+			exitGracefully(errors.New(fileName + " already exist"))
+		}
+
+		model = strings.ReplaceAll(model, "$MODELNAME$", strcase.ToCamel(modelName))
+		model = strings.ReplaceAll(model, "$TABLENAME$", tableName)
+
+		err = copyDataToFile([]byte(model), fileName)
+		if err != nil {
+			exitGracefully(err)
+		}
 	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 
 require (
 	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
+	github.com/gertd/go-pluralize v0.2.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/iancoleman/strcase v0.3.0 // indirect
