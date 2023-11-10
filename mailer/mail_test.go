f37e0f3ec4c86e2858176b93c5d5a3431f152e59
Akshansh Gusain <akshanshgusain@gmail.com>
2023-11-10 12:02:29 +0530

    test(mailer): add TestMail_SendUsingChan test

--- mailer/mail_test.go
+++ mailer/mail_test.go
@@ -1,6 +1,9 @@
 package mailer
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestMail_SendSMTPMessage(t *testing.T) {
 	msg := Message{
@@ -15,5 +18,29 @@ func TestMail_SendSMTPMessage(t *testing.T) {
 	err := mailer.SendSMTPMessage(msg)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestMail_SendUsingChan(t *testing.T) {
+	msg := Message{
+		From:        "me@here.com",
+		FromName:    "Joe",
+		To:          "you@there.com",
+		Subject:     "test",
+		Template:    "test",
+		Attachments: []string{"./testdata/mail/test.html.tmpl"},
+	}
+
+	mailer.Jobs <- msg
+	res := <-mailer.Results
+	if res.Error != nil {
+		t.Error(errors.New("failed to send over channel"))
+	}
+
+	msg.To = "not_an_email_address"
+	mailer.Jobs <- msg
+	res = <-mailer.Results
+	if res.Error == nil {
+		t.Error(errors.New("no error received with invalid to address"))
 	}
 }
