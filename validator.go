a70e13199a67f4139bd601b55af7cc184ad3bc4b
Akshansh Gusain <akshanshgusain@gmail.com>
2024-09-14 11:00:25 +0530

    refactor: move funcs and add comments for better readablity

--- validator.go
+++ validator.go
@@ -9,17 +9,17 @@ import (
 	"time"
 )
 
-// Validation Easy validation for Form-Post
-type Validation struct {
-	Data   url.Values // this is standard for query params and form data
-	Errors map[string]string
-}
-
 func (j *January) Validator(data url.Values) *Validation {
 	return &Validation{
 		Data:   data,
 		Errors: make(map[string]string),
 	}
+}
+
+// Validation Easy validation for Form-Post
+type Validation struct {
+	Data   url.Values // this is standard for query params and form data
+	Errors map[string]string
 }
 
 func (v *Validation) Valid() bool {
